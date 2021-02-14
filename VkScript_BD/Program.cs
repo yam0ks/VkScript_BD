fcb1ebc616f52931f805ca0c0d5f344c4459cd4c
ViolentWorld <maxmtrushin@gmail.com>
2021-02-14 17:25:15 +0300

    Final Version

--- VkScript_BD/Program.cs
+++ VkScript_BD/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace VkScript_BD
 {
     class Program
     {
+        static List<string> ReadData()
+        {
+            List<string> Data = new List<string>();
+
+            using (StreamReader sr = new StreamReader("..\\..\\..\\DATA.txt", System.Text.Encoding.Default))
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    Data.Add(sr.ReadLine());
+                }
+            }
+
+            return Data;
+        }
+
         public static void Log(string Message)
         {
             File.AppendAllText("..\\..\\..\\log.txt", Message);
@@ -15,10 +31,11 @@ namespace VkScript_BD
         {
             try
             {
-                VkApi VkApi = new VkApi(81981776, "87e937f094f8a32f9f0c0e1f61e8a13c5edb98a4b2c404ecc75d897cfb51e86fe3509eacc1707be9f4ec0", "5.126");
-                await VkApi.MakeFinalRequest(VkApi.GetFinalMessage()); 
+                List<string> Data= ReadData();
+                VkApi VkApi = new VkApi(int.Parse(Data[0]), Data[1], Data[2]);
+                await VkApi.MakeFinalRequest(VkApi.GetFinalMessage());
             }
-            catch (Exception Exception) { Log("\n" + DateTime.Now.ToString() + " - " + Exception.Message + "\n" + Exception.StackTrace + "\n"); }
+            catch (Exception Exception) { Log(DateTime.Now.ToString() + " - " + Exception.Message + "\n" + Exception.StackTrace + "\n\n\n"); }
         }
     }
 }
